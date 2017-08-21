111f06c4265aeb614799c6c551a3aa784834cf73
minqi_jl <13986147769@139.com>
2017-08-21 18:24:21 +0800

    修改后台首页，增加错误页面重定向

--- WebRoot/backStage/js/index.js
+++ WebRoot/backStage/js/index.js
@@ -1,12 +1,35 @@
-//JavaScript代码区域
 layui.use('element', function(){
 	var element = layui.element;
 	var $ = layui.jquery;
 	element.init(); //这样element对动态生成的元素才会重新有效
 });
 
-var $ = layui.jquery;
+//根据登陆的账号，找到所属的菜单列表，代办，头像信息数据
 
-$('#lock').on('click', function() {
-	alert("lock点击了");
+
+//将数据打入页面
+
+
+
+/**
+ * 导航的点击事件，替换iframe的内容
+ * 注意：
+ * 1.jumpUrl测试环境是/marshow/xx/x.html；而正是环境无需项目名，即/xx/x.html
+ * 2.正式环境运行缓存，去掉Math的随机数
+ */
+$('a').on('click', function() {
+	var jumpUrl = $(this).attr("jumpUrl");
+	if(!form.isNull(jumpUrl)){
+		$("#iframe").attr("src", jumpUrl+"?rid="+Math.random());
+	}else{
+//		alert("不需要切换iframe里的内容");
+	}
 });
+
+
+$('#logOut').on('click', function() {
+	alert("logOut点击了");
+});
+
+
+
