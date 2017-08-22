00bba65b8135b07b7f9d4a1269c4717bd0dc3f09
minqi_jl <13986147769@139.com>
2017-08-22 10:21:16 +0800

    增加后台页面手机自适应样式

--- WebRoot/backStage/js/index.js
+++ WebRoot/backStage/js/index.js
@@ -11,6 +11,28 @@ layui.use('element', function(){
 
 
 
+//iframe自适应
+$(window).on('resize', function() {debugger
+	var $content = $('.admin-nav-card .layui-tab-content');
+	$content.height($(this).height() - 147);
+	$content.find('iframe').each(function() {
+		$(this).height($content.height());
+	});
+}).resize();
+
+//手机设备的简单适配
+var treeMobile = $('.site-tree-mobile');
+var shadeMobile = $('.site-mobile-shade');
+treeMobile.on('click', function() {
+	var $foot = $(".layui-footer");
+	$foot.attr("class","layui-hide");
+	$('body').addClass('site-mobile');
+});
+shadeMobile.on('click', function() {
+	$('body').removeClass('site-mobile');
+});
+
+
 /**
  * 导航的点击事件，替换iframe的内容
  * 注意：
