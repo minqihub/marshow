1715c84a92bc08da94cb404d7cfea5494cfee4ba
minqi_home <1019377970@qq.com>
2017-08-28 23:52:52 +0800

    一口气学了framework7的用法，做好移动端首页50%，这框架确实厉害

--- WebRoot/mobile/js/index.js
+++ WebRoot/mobile/js/index.js
@@ -1,12 +1,49 @@
 //http://framework7.taobao.org/docs/app-layout.html#.WaPtSpFJLp4
-var myApp = new Framework7();
-   
+var myApp = new Framework7({
+    swipePanel: 'right'		//滑动打开侧边栏
+});
 
-// If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
    
 // Add view
 var mainView = myApp.addView('.view-main', {
 	// Because we want to use dynamic navbar, we need to enable it for this view:
-	dynamicNavbar: true
+//	dynamicNavbar: true
+});
+
+//轮播图
+var mySwiper = myApp.swiper('.swiper-container', {
+    speed: 1000,
+    spaceBetween: 50,
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    autoplay: 3000,
+    loop: true
+});
+
+//下拉刷新
+var ptrContent = $$('.pull-to-refresh-content');
+ptrContent.on('refresh', function (e) {
+    setTimeout(function () {
+        myApp.pullToRefreshDone();
+    }, 2000);
+});
+
+
+
+
+$$('.alert-text-title-callback').on('click', function () {
+    myApp.alert('这是弹窗的内容！！！！', '这是弹窗的标题', function () {
+        myApp.alert('你点击了ok');
+    });
+});
+
+$$('.confirm-title-ok-cancel').on('click', function () {
+	myApp.confirm('Are you sure?', 'Custom Title', 
+	function () {
+		myApp.alert('You clicked Ok button');
+	},
+	function () {
+		myApp.alert('You clicked Cancel button');
+	});
 });
