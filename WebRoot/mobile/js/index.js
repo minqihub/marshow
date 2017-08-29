97b850af98e0ed846f1e79ff01bd29cd4d9057d3
minqi_jl <13986147769@139.com>
2017-08-29 17:38:08 +0800

    加入icon库文件；f7初始化的相关配置；移动端首页布局完成70%，f7威武

--- WebRoot/mobile/js/index.js
+++ WebRoot/mobile/js/index.js
@@ -1,6 +1,10 @@
 //http://framework7.taobao.org/docs/app-layout.html#.WaPtSpFJLp4
 var myApp = new Framework7({
-    swipePanel: 'right'		//滑动打开侧边栏
+    swipePanel: 'right',		//滑动打开侧边栏
+    material: true,				//确定主题
+    modalTitle: "提示",			//弹窗的相关配置(Alert, Confirm, Prompt)
+    modalButtonOk: "确定",
+    modalButtonCancel: "取消",
 });
 
 var $$ = Dom7;
@@ -21,6 +25,12 @@ var mySwiper = myApp.swiper('.swiper-container', {
     loop: true
 });
 
+//轮播图点击事件
+$$('.swiper-slide').on('click', function () {
+	window.location.href = "http://www.baidu.com";
+});
+
+
 //下拉刷新
 var ptrContent = $$('.pull-to-refresh-content');
 ptrContent.on('refresh', function (e) {
@@ -33,17 +43,45 @@ ptrContent.on('refresh', function (e) {
 
 
 $$('.alert-text-title-callback').on('click', function () {
-    myApp.alert('这是弹窗的内容！！！！', '这是弹窗的标题', function () {
+    myApp.alert('这是弹窗的内容！！！！', function () {
         myApp.alert('你点击了ok');
     });
 });
 
 $$('.confirm-title-ok-cancel').on('click', function () {
-	myApp.confirm('Are you sure?', 'Custom Title', 
+	myApp.confirm('您确定这么做吗？', 
 	function () {
 		myApp.alert('You clicked Ok button');
 	},
 	function () {
 		myApp.alert('You clicked Cancel button');
 	});
+});
+
+//定位
+$$('.left.sliding').on('click', function () {
+	myApp.alert('待开发，定位功能需要调用微信JS-SDK...', '提示');
+});
+
+//扫码
+$$('.right.sliding').on('click', function () {
+	myApp.alert('待开发，扫码功能需要调用微信JS-SDK...', '提示');
+});
+
+
+//底部工具栏切换效果
+$$('.tab-link').on('click', function () {debugger
+	var aaa = $$(this);
+	var others = aaa.siblings();
+	for ( var i = 0; i < others.length; i++) {
+		var oldClassName = others[i].className;
+		if(oldClassName.indexOf("active") != -1){
+			others[i].className = "tab-link";
+		}else if(oldClassName.indexOf("open-panel") != -1){
+			others[i].className = "tab-link open-panel";
+		}
+	}
+	var oldClass = aaa[0].className;
+	aaa.attr("class", oldClass + " active");
+    console.info("afaefeasfasf");
 });
