73f8dbc87ab09b787f604a4f35e55223b1e40ef2
minqi_jl <13986147769@139.com>
2017-09-04 17:46:07 +0800

    增加log4j配置

--- src/com/framework/utils/SysLog.java
+++ src/com/framework/utils/SysLog.java
@@ -1,5 +1,7 @@
 package com.framework.utils;
 
+import org.apache.log4j.Logger;
+
 /**
  * 系统日志类
  * @author 2017-07-29 12:35:18
@@ -7,6 +9,7 @@ package com.framework.utils;
  */
 public class SysLog {
 	
+	protected Logger log = Logger.getLogger(SysLog.class);
 	
 	/**
 	 * 控制台打印日志
@@ -16,9 +19,6 @@ public class SysLog {
 	public static void log(Class clazz, String info) {
 		System.out.println("["+DataUtils.getSysTime()+"  "+clazz+"]【"+info+"】");
 	}
-	
-	
-	
 	
 	
 	
