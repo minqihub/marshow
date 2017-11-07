f2183ecf917633ac26941e4331eea0559f6b465a
minqi_jl <13986147769@139.com>
2017-11-07 14:09:16 +0800

    日常修改

---
+++ src/com/framework/aop/Invoke.java
@@ -0,0 +1,10 @@
+package com.framework.aop;
+
+/**
+ * 外部接口路由
+ * @author minqi 2017-11-07 09:06:04
+ *
+ */
+public class Invoke {
+
+}
---
+++ src/com/user/UserRole.java
@@ -0,0 +1,10 @@
+package com.user;
+
+/**
+ * 用户角色、权限
+ * @author minqi 2017-11-07 09:10:58
+ *
+ */
+public class UserRole {
+
+}
