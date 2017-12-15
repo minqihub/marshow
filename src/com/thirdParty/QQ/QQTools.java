e7f0142337db6d74b02311d608edcf5804577c72
minqi_jl <13986147769@139.com>
2017-12-15 19:53:59 +0800

    增加qq开放平台接口

---
+++ src/com/thirdParty/QQ/QQTools.java
@@ -0,0 +1,10 @@
+package com.thirdParty.QQ;
+
+/**
+ * QQ开放接口
+ * http://wiki.open.qq.com/wiki/%E7%BD%91%E7%AB%99%E6%8E%A5%E5%85%A5
+ * @author minqi 2017-12-15 16:11:57
+ */
+public class QQTools {
+
+}
